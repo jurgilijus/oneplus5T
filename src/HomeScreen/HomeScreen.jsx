920f2c634c9b732f9f564c236d09adff3179d238
jurgilijus <jurgissubacius@gmail.com>
2023-03-06 15:56:53 +0200

    first commit

---
+++ src/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// CSS
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div className="home-screen-conteiner">
+      <div className="home-screen"></div>
+    </div>
+  );
+}
+
+export default HomeScreen;
